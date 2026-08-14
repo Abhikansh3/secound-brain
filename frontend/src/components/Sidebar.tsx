eb2d5b147d6b2ef554206766e1bbd0a90b32756b
Abhikansh Mittal <abhikansh33@gmail.com>
2026-08-14 11:18:29 +0530

    made changes

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -7,7 +7,9 @@ export function Sidebar() {
   return <div className="w-76 h-full bg-white border-r  p-4">
     <div className="flex flex-col pl-2 pt-2">
       <div className="flex items-center gap-4">
-        <Logo />
+        <div className="text-purple-600">
+          <Logo />
+        </div>
         <div className="text-2xl">Brainly</div>
       </div>
 
